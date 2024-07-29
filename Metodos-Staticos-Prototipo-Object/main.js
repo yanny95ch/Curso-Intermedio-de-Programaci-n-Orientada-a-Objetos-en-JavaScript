2eec196e6dcaf6bf9c0c8eddc7ef86e5ccd2037c
yannych1995 <yannymaly@gmal.com>
2024-07-29 12:44:28 -0500

    Object.defineProperty

--- Metodos-Staticos-Prototipo-Object/main.js
+++ Metodos-Staticos-Prototipo-Object/main.js
@@ -8,17 +8,46 @@ const juan = {
         this.approvedCourses.push(newCourse);
     }
 };
+//  console.log(Object.keys(juan));
+//  console.log(Object.getOwnPropertyNames(juan));
+//  console.log(Object.entries(juan)); 
+//---------------------------------------//-----------------------------
 
-console.log(Object.keys(juan));
-console.log(Object.getOwnPropertyNames(juan));
-console.log(Object.entries(juan)); 
 
-console.log(Object.getOwnPropertyDescriptors(juan));
+//Object.defineProperty : 
 
-Object.defineProperty(juan, 'PruebaNasa',{
-    value: 'estraterrestres',
+Object.defineProperty(juan, 'navigator',{
+    value: 'Chrome',
     writable : true,
     configurable:true,
-    enumerable:true
-
+    enumerable:false
+//enumerable: La propiedad no aparesca  con keys pero si con  getOwnPropertyNames
 })
+Object.defineProperty(juan, 'editor',{
+    value: 'vscode',
+    writable : false,
+    configurable:true,
+    enumerable:true
+//writable:  La propiedad aparece con keys y con getOwnPropertyNames pero no se puede editar, pero si se puede borrar 
+})
+
+Object.defineProperty(juan, 'terminal',{
+    value: 'WSL',
+    writable : true,
+    configurable:false,
+    enumerable:true
+//  configurable: La propiedad aparece con keys y con getOwnPropertyNames, la podemos editar pero no se puede borrar 
+})
+Object.defineProperty(juan, 'PuebaNAsa',{
+    value: 'extraterrestres',
+    writable : false,
+    configurable:false,
+    enumerable:false
+//todas las propiedades en false: no se puede borrar  , no se puede editar , La propiedad no aparesca  con keys pero si con  getOwnPropertyNames
+})
+
+
+// Object.seal(juan); //Evita que las propiedades se puedan borrar: es decir  que la pripoedad configurable: false
+Object.freeze(juan); //configurable: false  y  writable : false todas las propiedades ya se no se pueadn borrar o sobreescribir el dato,
+
+console.log(Object.getOwnPropertyDescriptors(juan));
